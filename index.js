8b3eac171a0cdde7bce9c79eacbe53d7988dd91a
user <user@example.com>
2023-03-26 18:55:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,39 @@
 import fetch from 'chiaki'
 
 
-export default class Replicate {
+export default {
+	run: async function(model, inputs) {
+		let prediction = await this.create(model, inputs)
 
-        async run(model, inputs) {
-                let prediction = await this.create(model, inputs)
+		while (! [
+			'canceled',
+			'succeeded',
+			'failed'
+		].includes(prediction.status)) {
+			await new Promise(_ => setTimeout(_, 250))
+			prediction = await this.get(prediction)
+		}
 
-                while (! [
-                        'canceled',
-                        'succeeded',
-                        'failed'
-                ].includes(prediction.status)) {
-                        await new Promise(_ => setTimeout(_, 250))
-                        prediction = await this.get(prediction)
-                }
+		return prediction.output
+	},
+	
+	get(prediction) {
+		return fetch(`https://replicate.com/api/models${prediction.version.model.absolute_url}/versions/${prediction.version_id}/predictions/${prediction.uuid}`)
+			.then(response => JSON.parse(response.body).prediction)
+	},
 
-                return prediction
-        }
+	create(model, inputs) {
+		const [path, version] = model.split(':')
 
-        async get(prediction) {
-                return await fetch(`https://replicate.com/api/models${prediction.version.model.absolute_url}/versions/${prediction.version_id}/predictions/${prediction.uuid}`)
-                        .then(response => JSON.parse(response.body).prediction)
-        }
-
-        async create(model, inputs) {
-                const [path, version] = model.split(':')
-
-                return await fetch({
-                        hostname: 'replicate.com',
-                        path: `/api/models/${path}/versions/${version}/predictions`,
-                        method: 'POST',
-                        headers: {
-                                'content-type': 'application/json',
-                        },
-                        body: JSON.stringify({ inputs }),
-                })
-                .then(response => JSON.parse(response.body))
-        }
+		return fetch({
+			hostname: 'replicate.com',
+			path: `/api/models/${path}/versions/${version}/predictions`,
+			method: 'POST',
+			headers: {
+				'content-type': 'application/json',
+			},
+			body: JSON.stringify({ inputs }),
+		})
+		.then(response => JSON.parse(response.body))
+	}
 }
